7c8fbe4093a6aa932670db56840d5e5bdded5318
agogo233 <276149387+agogo233@users.noreply.github.com>
2026-06-13 02:32:18 +0000

    feat: port 31115~31119, rename RustDesk → MyDesk

--- src/hbbr.rs
+++ src/hbbr.rs
@@ -20,7 +20,7 @@ fn main() -> ResultType<()> {
     let matches = App::new("hbbr")
         .version(version::VERSION)
         .author("Purslane Ltd. <info@rustdesk.com>")
-        .about("RustDesk Relay Server")
+        .about("MyDesk Relay Server")
         .args_from_usage(&args)
         .get_matches();
     if let Ok(v) = ini::Ini::load_from_file(".env") {
--- src/utils.rs
+++ src/utils.rs
@@ -110,11 +110,11 @@ fn doctor_ip(server_ip_address: std::net::IpAddr, server_address: Option<&str>) 
 
     // port check TCP (UDP is hard to check)
     doctor_tcp(server_ip_address, "21114", "API");
-    doctor_tcp(server_ip_address, "21115", "hbbs extra port for nat test");
-    doctor_tcp(server_ip_address, "21116", "hbbs");
-    doctor_tcp(server_ip_address, "21117", "hbbr tcp");
-    doctor_tcp(server_ip_address, "21118", "hbbs websocket");
-    doctor_tcp(server_ip_address, "21119", "hbbr websocket");
+    doctor_tcp(server_ip_address, "31115", "hbbs extra port for nat test");
+    doctor_tcp(server_ip_address, "31116", "hbbs");
+    doctor_tcp(server_ip_address, "31117", "hbbr tcp");
+    doctor_tcp(server_ip_address, "31118", "hbbs websocket");
+    doctor_tcp(server_ip_address, "31119", "hbbr websocket");
 
     // TODO: key check
 }
